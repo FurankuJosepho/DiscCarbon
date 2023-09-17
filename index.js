645f40c41c59adc1b5f699a6b523a1f41132c8a3
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-18 00:05:38 +0800

    Changes

--- index.js
+++ index.js
@@ -69,13 +69,24 @@ async function openClose() {
     console.error("Error accessing the camera:", error);
   }
 }
-
+function myAlert() {
+    alert("Press Any of the Keys Like ( q , w , e , r , a , s , d , f , z , x , c )")
+}
 
 document.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "a":
       audio1.play();
       audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
       audio1.currentTime = 0;
       let Wow = document.getElementById("Wow");
       Wow.style.background = "Grey";
@@ -84,8 +95,17 @@ document.addEventListener("keydown", (e) => {
       }, 1500);
       break;
     case "s":
-      audio2.play();
       audio1.pause();
+      audio2.play();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
       audio2.currentTime = 0;
       let Bob = document.getElementById("Bob");
       Bob.style.background = "Grey";
@@ -94,21 +114,36 @@ document.addEventListener("keydown", (e) => {
       }, 1500);
       break;
     case "d":
-      audio3.play();
-      audio2.pause();
       audio1.pause();
-      audio2.currentTime = 0;
+      audio2.pause();
+      audio3.play();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
+      audio3.currentTime = 0;
       let Blue = document.getElementById("Blue");
       Blue.style.background = "Grey";
       setTimeout(() => {
         Blue.style.background = "#26292c";
       }, 1500);
       break;
-    case "w":
-      audio4.play();
-      audio3.pause();
-      audio2.pause();
+    case "f":
       audio1.pause();
+      audio2.pause();
+      audio3.pause();
+      audio4.play();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
       audio4.currentTime = 0;
       let Pew = document.getElementById("Pew");
       Pew.style.background = "Grey";
@@ -116,36 +151,140 @@ document.addEventListener("keydown", (e) => {
         Pew.style.background = "#26292c";
       }, 1500);
       break;
-    case "w":
-        audio6.play();
-      audio5.pause();
-      audio4.pause();
-      audio3.pause();
-      audio2.pause();
+    case "q":
       audio1.pause();
-      audio6.currentTime = 0;
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.play();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
+      audio5.currentTime = 0;
       let Fart = document.getElementById("Fart");
       Fart.style.background = "Grey";
       setTimeout(() => {
         Fart.style.background = "#26292c";
       }, 1500);
       break;
-    case "q":
-        audio7.play()
-      audio6.pause();
-      audio5.pause();
-      audio4.pause();
-      audio3.pause();
-      audio2.pause();
+    case "w":
       audio1.pause();
-      audio7.currentTime = 0;
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.play();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
+      audio6.currentTime = 0;
       let Devu = document.getElementById("Devu");
       Devu.style.background = "Grey";
       setTimeout(() => {
         Devu.style.background = "#26292c";
       }, 1500);
       break;
-      default:
-        break;
+    case "e":
+      audio1.pause();
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.play();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
+      audio7.currentTime = 0;
+      let Emotional = document.getElementById("Emotional");
+      Emotional.style.background = "Grey";
+      setTimeout(() => {
+        Emotional.style.background = "#26292c";
+      }, 1500);
+      break;
+    case "r":
+      audio1.pause();
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.play();
+      audio9.pause();
+      audio10.pause();
+      audio11.pause();
+      audio8.currentTime = 0;
+      let Runner = document.getElementById("Run");
+      Runner.style.background = "Grey";
+      setTimeout(() => {
+        Runner.style.background = "#26292c";
+      }, 1500);
+      break;
+    case "z":
+      audio1.pause();
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.play();
+      audio10.pause();
+      audio11.pause();
+      audio9.currentTime = 0;
+      let Sadest = document.getElementById("Sad");
+      Sadest.style.background = "Grey";
+      setTimeout(() => {
+        Sadest.style.background = "#26292c";
+      }, 1500);
+      break;
+    case "x":
+      audio1.pause();
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.play();
+      audio11.pause();
+      audio10.currentTime = 0;
+      let tutut = document.getElementById("tutut");
+      tutut.style.background = "Grey";
+      setTimeout(() => {
+        tutut.style.background = "#26292c";
+      }, 1500);
+      break;
+    case "c":
+      audio1.pause();
+      audio2.pause();
+      audio3.pause();
+      audio4.pause();
+      audio5.pause();
+      audio6.pause();
+      audio7.pause();
+      audio8.pause();
+      audio9.pause();
+      audio10.pause();
+      audio11.play();
+      audio11.currentTime = 0;
+      let Who = document.getElementById("Who");
+      Who.style.background = "Grey";
+      setTimeout(() => {
+        Who.style.background = "#26292c";
+      }, 1500);
+      break;
+    default:
+      break;
   }
 });
